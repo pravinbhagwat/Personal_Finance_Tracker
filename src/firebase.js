3d0b6cff988c552996aedbd06b42a30375a728d1
Pravin <pravinbhagwat150@gmail.com>
2023-09-20 14:58:46 +0530

    Config updated

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAHExHfMDRu2lVecgYxgW86TXDF-dvsWrY",
-  authDomain: "personal-finance-tracker-ec56e.firebaseapp.com",
-  projectId: "personal-finance-tracker-ec56e",
-  storageBucket: "personal-finance-tracker-ec56e.appspot.com",
-  messagingSenderId: "669303047696",
-  appId: "1:669303047696:web:5b4b41cc896752c03e6c95",
-  measurementId: "G-7X36E69HYW",
+  apiKey: "AIzaSyAI75NfcSSF3qTCJhyQU95xHLB8oOUMQj8",
+  authDomain: "personal-finance-tracker-18b60.firebaseapp.com",
+  projectId: "personal-finance-tracker-18b60",
+  storageBucket: "personal-finance-tracker-18b60.appspot.com",
+  messagingSenderId: "895554635166",
+  appId: "1:895554635166:web:284f410fdb7ab8b2a9c28f",
+  measurementId: "G-V0K5WPXLHP",
 };
 
 const app = initializeApp(firebaseConfig);
